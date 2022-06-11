641a771c41b71dcfd0b8ffe2349b573ad97524d7
itsPeetah <pietro.g.moroni@gmail.com>
2022-06-11 20:06:14 +0200

    Create stream-over.tsx

---
+++ pages/stream-over.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const StreamOver: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 text-white">
+      <Head>
+        <title>Starting Soon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="fixed min-h-screen min-w-full bg-black -z-50" />
+      <Image
+        className=" -z-10 opacity-50"
+        src="/images/background/celeste_farewell.png"
+        layout="fill"
+      />
+      <h1 className="md:text-xl lg:text-3xl font-extralight">
+        stream ending soon...
+      </h1>
+      <h1 className="md:text-6xl lg:text-8xl animate-bounce-slow font-extralight italic mb-20 mt-12">
+        thank you for watching
+      </h1>
+    </div>
+  );
+};
+
+export default StreamOver;
