17acc79604b9314291269315503203197dc65c2e
itsPeetah <pietro.g.moroni@gmail.com>
2022-06-11 19:53:06 +0200

    Starting soon

---
+++ pages/starting-soon.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const StartingSoon: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 text-white">
+      <Head>
+        <title>Starting Soon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="fixed min-h-screen min-w-full bg-black -z-50" />
+      <Image
+        className=" -z-10 opacity-50"
+        src="/images/background/celeste_farewell.png"
+        layout="fill"
+      />
+      <h1 className="md:text-xl lg:text-3xl font-thin">
+        thank you for tuning in
+      </h1>
+      <h1 className="md:text-7xl lg:text-9xl animate-bounce-slow font-thin italic mb-20 mt-12">
+        starting soon
+      </h1>
+      <h1 className="md:text-2xl lg:text-4xl animate-pulse-slow font-thin">
+        grab a snack and enjoy
+      </h1>
+    </div>
+  );
+};
+
+export default StartingSoon;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "bounce-slow": "pan-y 5s ease-in-out infinite",
+        "pulse-slow": "pulse 1s infinite",
+      },
+      keyframes: {
+        "pan-y": {
+          "0%, 100%": {
+            transform: "translateY(-25%)",
+          },
+          "50%": {
+            transform: "translateY(25%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
